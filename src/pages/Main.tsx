c09f90e002cbd327c5f49459c2f158d0ab617915
Olha Popova <fc.odessa.10@gmail.com>
2025-06-09 22:09:51 -0700

    fix: change a chref for link element

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const MainPage = ( ) => {
 	return (
 		<div className="bg-gradient-to-br from-pink-50 via-white to-indigo-100 min-h-screen flex items-center justify-center px-4 py-10">
@@ -7,19 +9,19 @@ const MainPage = ( ) => {
 			</h1>
 
 			<nav className="space-y-6">
-				<a
-					href="./components/Task-1.tsx"
+				<Link 
+					to="./components/Task-1.tsx"
 					className="block rounded-xl p-5 bg-indigo-500 text-white text-xl font-semibold shadow-md hover:bg-indigo-600 transition duration-300 ease-in-out"
 				>
 					 Task 1
-				</a>
+				</Link>
 
-				<a
-					href="./components/Task-2.tsx"
+				<Link
+					to="./components/Task-2.tsx"
 					className="block rounded-xl p-5 bg-pink-500 text-white text-xl font-semibold shadow-md hover:bg-pink-600 transition duration-300 ease-in-out"
 				>
 					Task 2
-				</a>
+				</Link>
 			</nav>
 
 			<p className="text-center text-#8e8c8c-500 text-sm mt-10">
