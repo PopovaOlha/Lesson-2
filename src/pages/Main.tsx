52b8d551cad577152026be899478dec0a2d78b53
Olha Popova <fc.odessa.10@gmail.com>
2025-06-09 22:06:26 -0700

    feat: add react router dom

---
+++ src/pages/Main.tsx
@@ -0,0 +1,32 @@
+const MainPage = ( ) => {
+	return (
+		<div className="bg-gradient-to-br from-pink-50 via-white to-indigo-100 min-h-screen flex items-center justify-center px-4 py-10">
+		<div className="max-w-xl w-full bg-white rounded-2xl shadow-xl p-8">
+			<h1 className="text-4xl font-extrabold text-center text-indigo-700 mb-8 tracking-tight">
+				📘 My React Tasks
+			</h1>
+
+			<nav className="space-y-6">
+				<a
+					href="./components/Task-1.tsx"
+					className="block rounded-xl p-5 bg-indigo-500 text-white text-xl font-semibold shadow-md hover:bg-indigo-600 transition duration-300 ease-in-out"
+				>
+					 Task 1
+				</a>
+
+				<a
+					href="./components/Task-2.tsx"
+					className="block rounded-xl p-5 bg-pink-500 text-white text-xl font-semibold shadow-md hover:bg-pink-600 transition duration-300 ease-in-out"
+				>
+					Task 2
+				</a>
+			</nav>
+
+			<p className="text-center text-#8e8c8c-500 text-sm mt-10">
+				Built with 💙 by Olha
+			</p>
+		</div>
+	</div>
+	);
+}
+export default MainPage;
