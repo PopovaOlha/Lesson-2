331be97ae2370ac5101f08d1cc1d4014b8af2979
Olha Popova <fc.odessa.10@gmail.com>
2025-06-09 22:24:03 -0700

    fix: eslint.config

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './pages/Main'
+import Main from './pages/Main';
 
 function App() {
   return (
-   <BrowserRouter>
-   <Routes>
-    <Route path='/' element={<Main />}/>
-   </Routes>
-   </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,34 +1,34 @@
 import { Link } from 'react-router-dom';
 
-const MainPage = ( ) => {
-	return (
-		<div className="bg-gradient-to-br from-pink-50 via-white to-indigo-100 min-h-screen flex items-center justify-center px-4 py-10">
-		<div className="max-w-xl w-full bg-white rounded-2xl shadow-xl p-8">
-			<h1 className="text-4xl font-extrabold text-center text-indigo-700 mb-8 tracking-tight">
-				📘 My React Tasks
-			</h1>
+const MainPage = () => {
+  return (
+    <div className="bg-gradient-to-br from-pink-50 via-white to-indigo-100 min-h-screen flex items-center justify-center px-4 py-10">
+      <div className="max-w-xl w-full bg-white rounded-2xl shadow-xl p-8">
+        <h1 className="text-4xl font-extrabold text-center text-indigo-700 mb-8 tracking-tight">
+          📘 My React Tasks
+        </h1>
 
-			<nav className="space-y-6">
-				<Link 
-					to="./components/Task-1.tsx"
-					className="block rounded-xl p-5 bg-indigo-500 text-white text-xl font-semibold shadow-md hover:bg-indigo-600 transition duration-300 ease-in-out"
-				>
-					 Task 1
-				</Link>
+        <nav className="space-y-6">
+          <Link
+            to="./components/Task-1.tsx"
+            className="block rounded-xl p-5 bg-indigo-500 text-white text-xl font-semibold shadow-md hover:bg-indigo-600 transition duration-300 ease-in-out"
+          >
+            Task 1
+          </Link>
 
-				<Link
-					to="./components/Task-2.tsx"
-					className="block rounded-xl p-5 bg-pink-500 text-white text-xl font-semibold shadow-md hover:bg-pink-600 transition duration-300 ease-in-out"
-				>
-					Task 2
-				</Link>
-			</nav>
+          <Link
+            to="./components/Task-2.tsx"
+            className="block rounded-xl p-5 bg-pink-500 text-white text-xl font-semibold shadow-md hover:bg-pink-600 transition duration-300 ease-in-out"
+          >
+            Task 2
+          </Link>
+        </nav>
 
-			<p className="text-center text-#8e8c8c-500 text-sm mt-10">
-				Built with 💙 by Olha
-			</p>
-		</div>
-	</div>
-	);
-}
+        <p className="text-center text-#8e8c8c-500 text-sm mt-10">
+          Built with 💙 by Olha
+        </p>
+      </div>
+    </div>
+  );
+};
 export default MainPage;
